d6d0b75a1a36a941cb3e7518c868bda68c4b414c
adhityaramadhanus <adhitya.ramadhanus@gmail.com>
2018-08-14 19:03:13 +0700

    move prefix key to options, accomodate options, wrap error

--- minerva.go
+++ minerva.go
@@ -2,52 +2,71 @@ package minerva
 
 import (
 	"context"
-	"log"
+	"fmt"
+
+	"github.com/pkg/errors"
 )
+
+type Options struct {
+	PrefixKey string
+}
+
+var defaultOptions = Options{
+	PrefixKey: "config",
+}
 
 type Minerva struct {
 	remoteClient RemoteClient
 	remoteConfig map[string]string
 	cancelFunc   context.CancelFunc
 	context      context.Context
+	options      Options
 }
 
 func New(remoteClient RemoteClient) *Minerva {
 	return &Minerva{
 		remoteClient: remoteClient,
 		remoteConfig: map[string]string{},
+		options:      defaultOptions,
+	}
+}
+
+func NewWithOptions(remoteClient RemoteClient, options Options) *Minerva {
+	return &Minerva{
+		remoteClient: remoteClient,
+		remoteConfig: map[string]string{},
+		options:      options,
 	}
 }
 
 func (m *Minerva) Get(key string) string {
 	_, isKeyPresent := m.remoteConfig[key]
+	remoteConfigKey := fmt.Sprintf("%s:%s", m.options.PrefixKey, key)
 	if !isKeyPresent {
-		m.remoteConfig[key] = m.remoteClient.Get("config:" + key)
+		m.remoteConfig[key] = m.remoteClient.Get(remoteConfigKey)
 	}
 	return m.remoteConfig[key]
 }
 
-// TODO: Add golang context
 func (m *Minerva) Watch() error {
 	ctx, cancel := context.WithCancel(context.Background())
 	m.context = ctx
 	m.cancelFunc = cancel
 
-	keyEventChannel, err := m.remoteClient.Watch(ctx)
+	keyEventChannel, err := m.remoteClient.Watch(ctx, m.options.PrefixKey)
 
 	go func(ctx context.Context) {
 		for {
 			select {
 			case keyEvent := <-keyEventChannel:
 				m.remoteConfig[keyEvent.AffectedKey] = keyEvent.Value
-				log.Println("Minerva Event:", keyEvent.Type, keyEvent.AffectedKey, keyEvent.Value)
 			case <-ctx.Done():
 				return
 			}
 		}
 	}(ctx)
 
-	return err
+	return errors.Wrap(err, "Error in watching key event")
 }
 
 func (m *Minerva) Close() {
